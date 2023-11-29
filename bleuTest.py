a66198a896457bcc611d9019f041b2595e8f2f15
Christina Gottschalk <christinaisa.g@gmail.com>
2023-11-29 17:30:45 +0100

    bleu tests

--- bleuTest.py
+++ bleuTest.py
@@ -1,9 +1,8 @@
 import evaluate
 
+# Lade die BLEU-Metrik
 bleu = evaluate.load("bleu")
-predictions = ["I have thirty six years. I also have great hair and I run really fast."]
-references = [
-    ["I am thirty six years old. I do have great hair an I'm the fastest runner in town."]
-]
+predictions = ["I have thirty six years old."]
+references = ["I'm thirty six years old."]
 results = bleu.compute(predictions=predictions, references=references)
 print(results)
