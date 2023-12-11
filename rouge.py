8579e787f531f318339f061ee483802d601ac60a
Yao Yao Zhu <82475029+yaoyao-zhu@users.noreply.github.com>
2023-12-11 12:31:53 +0100

    neues dataset angebunden

--- rouge.py
+++ rouge.py
@@ -4,21 +4,26 @@ from rouge_score import rouge_scorer
 scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
 
 # Pfad JSONL-Datei
-jsonl_file_path = "de_test.jsonl"
+json_file_path = "test.json"
 
 # Daten aus JSONL-Datei lesen
-with open(jsonl_file_path, 'r', encoding='utf-8') as file:
-    dataset = [json.loads(line) for line in file]
+with open(json_file_path, 'r', encoding='utf-8') as file:
+    lines = file.readlines()
 
 # Rouge-Scores für jeden Datensatzpunkt im Datensatz berechnen
-for data_point in dataset:
-    candidate_summary = data_point["candidate"] #Platzhalter für LLM output
-    reference_summary = data_point["summary"]
+for line in lines[:2]:
+    data_point = json.loads(line)
+    candidate_summary = data_point.get("wiki_sentences", [])  # Platzhalter für LLM output
+    reference_summary = data_point.get("klexikon_sentences", [])
 
-    scores = scorer.score(reference_summary, candidate_summary)
+    # Konvertiere die Listen in Strings
+    candidate_summary_str = " ".join(candidate_summary)
+    reference_summary_str = " ".join(reference_summary)
 
-    print(f"Candidate Summary: {candidate_summary}")
-    print(f"Reference Summary: {reference_summary}")
+    scores = scorer.score(reference_summary_str, candidate_summary_str)
+
+    print(f"Candidate Summary: {candidate_summary_str}")
+    print(f"Reference Summary: {reference_summary_str}")
 
     for key in scores:
         print(f'{key}: {scores[key]}')
