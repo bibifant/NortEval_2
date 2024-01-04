7f369e8c5774e52833c86c61536b61f67e57699a
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2024-01-04 10:58:22 +0100

    fehler gelöst

--- rouge.py
+++ rouge.py
@@ -2,30 +2,31 @@ import json
 from rouge_score import rouge_scorer
 import sys
 sys.path.append('script')
-from azure_openai_connection import get_answer
+from script.azure_openai_connection import get_answer
+
 
 def rouge():
     # Initialisiere den ROUGE-Scorer
     scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
 
-    # Pfad JSONL-Datei
+    # Pfad JSON-Datei
     json_file_path = "test.json"
 
     # Pfad für die Ausgabedatei
-    output_file_path = "llm_evaluation_rouge.txt"
+    output_file_path = "llm_evaluation_rougee.txt"
 
     # Öffne die Ausgabedatei im Schreibmodus
     with open(output_file_path, 'w', encoding='utf-8') as output_file:
-        # Lade die Daten aus der JSONL-Datei
+        # Lade die Daten aus der JSON-Datei
         with open(json_file_path, 'r', encoding='utf-8') as file:
             lines = file.readlines()
 
         # Rouge-Scores für jeden Datensatzpunkt im Datensatz berechnen
-        for index, line in enumerate(lines[:2]):
+        for index, line in enumerate(lines[:3]):
             data_point = json.loads(line)
 
             # Füge das Attribut "wiki_sentences" zum Prompt hinzu
-            prompt = f"Fasse den Text in 10 Sätzen in deutsch zusammen:\n{data_point['wiki_sentences']}"
+            prompt = f"Fasse den Text in 10 Sätzen in deutsch zusammen:\n{data_point.get('wiki_sentences')}"
 
             # Schreibe den Prompt mit Index in die Datei
             output_file.write(f"Index: {index}\n")
@@ -53,3 +54,7 @@ def rouge():
                 output_file.write(f'{key}: {scores[key]}\n')
 
             output_file.write("\n")
+
+
+# Aufruf der Funktion
+rouge()
