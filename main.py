f4d561fe19aba0f01b3dc2d46a3a922d168f68d3
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2024-01-05 16:33:43 +0100

    aufruf von rouge angepasst

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from de_en import run_de_en_test
 from functionGK import run_function_gk_test
 from gebeugtes_verb import run_gebeugtes_verb_test
 from perplexityTest import run_perplexity_test
-from rouge import run_rouge_test
+from rouge import run_rouge
 
 
 def main():
@@ -40,7 +40,7 @@ def main():
     run_perplexity_test(dataset_path_perplexity)
 
     print("\n\nRouge:")
-    run_rouge_test(jsonl_file_path_rouge)
+    run_rouge()
 
 
 if __name__ == "__main__":
