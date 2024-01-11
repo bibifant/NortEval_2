93838db05762049a819867ffa4cf64397e29633c
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2024-01-11 18:10:27 +0100

    aufgeräumt und unvollständige Funktionen auskommentiert

--- main.py
+++ main.py
@@ -1,48 +1,34 @@
-from create_results_file import create_results_file
-from bleu import run_bleu_test_on_json_dataset
-from de_en import run_de_en_test
-from functionGK import run_function_gk_test
-from gebeugtes_verb import run_gebeugtes_verb_test
-from perplexity_transformersGPT2 import run_perplexity_test
-from rouge import run_rouge
+from create_results import create_results
+from metrics.bleu import run_bleu_test_on_json_dataset
+from nlp.de_en import run_de_en_test
+from nlp.functionGK import run_function_gk_test
+from nlp.gebeugtes_verb import run_gebeugtes_verb_test
+from metrics.perplexity_transformersGPT2 import run_perplexity_test
+from metrics.rouge import run_rouge
 
 
 def main():
-    # Erstelle zuerst die results.json Datei
-    output_file = create_results_file()
+    #Erstelle zuerst die results.json Datei
+    output_file = create_results()
 
-    # Setze die Testdaten entsprechend
-    # bleu
-    json_file_bleu_path = "dataset/bleu_dataset.json"
-    output_file_path = "results/bleu_results.txt"
+    #Tests ausführen
+    #Bleu
+    #run_bleu_test_on_json_dataset()
 
-    example_text_de_en = "Dies ist an english example text und ein deutscher Text zum Überprüfen."
+    #Funktion deutsch-englisch
+    #run_de_en_test()
 
-    text_example_gk = "Der schnelle braune Fuchs springt über den faulen Hund und der Fuchs freut sich. Der große Baum."
+    #Funktion Groß- und Kleinschreibung
+    #run_function_gk_test()
 
-    sentences_gebeugtes_verb = ["Der Hund spielt im Park.",
-                                "Die Vögel singen fröhlich.",
-                                "lachen, spielen, singen.",
-                                "Fenster singen im Park."]
+    #Gebeugtes Verb
+    #run_gebeugtes_verb_test()
 
-    # Führe die Tests aus
-    print("\n\nBLEU:")
-    run_bleu_test_on_json_dataset(json_file_bleu_path, output_file_path)
-
-    print("\n\nde_en:")
-    run_de_en_test(example_text_de_en)
-
-    print("\n\nFunction GK:")
-    run_function_gk_test(text_example_gk)
-
-    print("\n\nGebeugtes Verb:")
-    run_gebeugtes_verb_test(sentences_gebeugtes_verb)
-
-    print("\n\nPerplexity:")
+    #Perplexity
     run_perplexity_test(output_file)
 
-    print("\n\nRouge:")
-    run_rouge()
+    #Rouge
+    run_rouge(output_file)
 
 
 if __name__ == "__main__":
