694795acf4be6b773712438dfd593f623be4a6aa
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2024-01-11 17:42:59 +0100

    results Ordner & avg_results file wird erstellt

---
+++ create_results.py
@@ -0,0 +1,31 @@
+import json
+import os.path
+from datetime import datetime
+
+def create_results():
+    # Aktuellen Zeitstempel im Format "YYYYMMDD-HHMMSS" erhalten
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    folder_name = f"results_{timestamp}"
+
+    # Überprüfe, ob der Ordner existiert. Wenn nicht, erstelle ihn.
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+    # Erstelle den Dateinamen für die Ergebnisdatei
+    file_name = os.path.join(folder_name, "avg_results.json")
+
+    data_structure = {
+        "Metrik Erklärungen": {
+            "Rouge": "Die Rouge-Metrik bewertet die Fähigkeit eines LLMs, Zusammenfassungen aus einem Input Text zu generieren. Der Rouge-Score reicht von 0 bis 1, wobei höhere Werte eine bessere Qualität der Zusammenfassung anzeigen.",
+            "BLEU": "BLEU ist eine Metrik zur Bewertung der Qualität von maschinellen Übersetzungen. Ein höherer BLEU-Score deutet darauf hin, dass die automatische Übersetzung besser mit den Referenzübersetzungen übereinstimmt",
+            "Perplexity": "Perplexity ist ein Maß für die Vorhersageunsicherheit eines Sprachmodells. Ein niedrigerer Wert deutet auf eine höhere Vorhersagegenauigkeit des Modells hin."
+        },
+        "Ergebnisse": []
+    }
+
+    with open(file_name, mode='w', encoding='utf-8') as file:
+        json.dump(data_structure, file, ensure_ascii=False, indent=4)
+    print(f"{file_name} wurde erstellt.")
+
+
+    return folder_name
