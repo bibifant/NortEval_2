b7fdc04ba1f270207bf1a06a2a6b2f2b2fa9253f
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2024-01-16 13:30:28 +0100

    angepasst

--- create_results.py
+++ create_results.py
@@ -3,15 +3,15 @@ import os.path
 from datetime import datetime
 
 def create_results():
-    # Aktuellen Zeitstempel im Format "YYYYMMDD-HHMMSS" erhalten
+    # Receive the current timestamp in the format "YYYYMMDD-HHMMSS"
     timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
     folder_name = f"results_{timestamp}"
 
-    # Überprüfe, ob der Ordner existiert. Wenn nicht, erstelle ihn.
+    # Check whether the folder exists. If not, create it.
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-    # Erstelle den Dateinamen für die Ergebnisdatei
+    # Create file name for the average result file
     file_name = os.path.join(folder_name, "avg_results.json")
 
     data_structure = {
@@ -20,11 +20,11 @@ def create_results():
             "BLEU": "BLEU ist eine Metrik zur Bewertung der Qualität von maschinellen Übersetzungen. Bleu Score reicht von 0 bis 1. Ein höherer BLEU-Score deutet darauf hin, dass die automatische Übersetzung besser mit den Referenzübersetzungen übereinstimmt.",
             "Perplexity": "Perplexity ist ein Maß für die Vorhersageunsicherheit eines Sprachmodells. Ein niedrigerer Wert deutet auf eine höhere Vorhersagegenauigkeit des Modells hin. Ein Wert nahe 50 ist hervorragend."
         },
-        "Ergebnisse": []
+        "Results": []
     }
 
     with open(file_name, mode='w', encoding='utf-8') as file:
         json.dump(data_structure, file, ensure_ascii=False, indent=4)
-    print(f"Der Ordner {folder_name} wurde erstellt.")
+    print(f"The folder {folder_name} has been created.")
 
     return folder_name
