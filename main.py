2419fc1df94ad91f53b3574b4a7de1d06ea8d788
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2024-01-19 22:01:43 +0100

    Kommentare auf englisch geändert

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from create_results import create_results
-#from metrics.bleu import run_bleu_test_on_json_dataset
-#from nlp.de_en import run_language_percentage
-#from nlp.upper_lower_case import upper_lower_case
+from metrics.bleu import run_bleu_test_on_json_dataset
+from nlp.de_en import run_language_percentage
+from nlp.upper_lower_case import run_upper_lower_case
 from nlp.conjugated_verb import run_conjugated_verb
-#from metrics.perplexity_transformersGPT2 import run_perplexity_test
-#from metrics.rouge import run_rouge
+from metrics.perplexity_transformersGPT2 import run_perplexity_test
+from metrics.rouge import run_rouge
 
 
 def main():
     # create the results folder with timestamp
     output_folder = create_results()
 
-    #Tests ausführen
     #Bleu
     #run_bleu_test_on_json_dataset()
 
-    #Funktion deutsch-englisch
-    #run_language_percentage(output_folder)
+    # Percentage of german & english words
+    run_language_percentage(output_folder)
 
-    #Funktion Groß- und Kleinschreibung
-    #upper_lower_case(output_folder)
+    # Upper lower case
+    run_upper_lower_case(output_folder)
 
     # Conjugated Verb
     run_conjugated_verb(output_folder)
 
     # Perplexity
-    # run_perplexity_test(output_folder)
+    run_perplexity_test(output_folder)
 
     # Rouge
-    #run_rouge(output_folder)
+    run_rouge(output_folder)
 
 
 if __name__ == "__main__":
