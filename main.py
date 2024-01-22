91566b0611a8129681fda4205c7663eca19041fb
Julia193 <Julia.Schultze@Student.HTW-Berlin.de>
2024-01-22 22:26:22 +0100

    ablauf geändert(erst metriken)

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from create_results import create_results
 from metrics.bleu import run_bleu_test_on_json_dataset
 from nlp.de_en import run_language_percentage
 from nlp.upper_lower_case import run_upper_lower_case
-from nlp.conjugated_verb import run_conjugated_verb
+from nlp.contains_verb import run_conjugated_verb
 from metrics.perplexity_transformersGPT2 import run_perplexity_test
 from metrics.rouge import run_rouge
 
@@ -12,7 +12,14 @@ def main():
     output_folder = create_results()
 
     #Bleu
+    #print("Bleu score is being calculated")
     #run_bleu_test_on_json_dataset()
+
+    # Perplexity
+    run_perplexity_test(output_folder)
+
+    # Rouge
+    run_rouge(output_folder)
 
     # Percentage of german & english words
     run_language_percentage(output_folder)
@@ -22,12 +29,6 @@ def main():
 
     # Conjugated Verb
     run_conjugated_verb(output_folder)
-
-    # Perplexity
-    run_perplexity_test(output_folder)
-
-    # Rouge
-    run_rouge(output_folder)
 
 
 if __name__ == "__main__":
