49814c17d192f788d68a41c3ace8e460d6262db3
Duong Nguyen <duongnt1010@gmail.com>
2024-02-04 16:31:49 +0100

    bias-detection: update bias with hate speech dataset, calculate the score of corect answer from openai, add result to output folder and add conclusion

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from create_results import create_results
 from metrics.bleu import calculate_bleu
-from nlp.sentiment_analysis import run_sentiment_analysis
+from metrics.perplexity_transformersGPT2 import run_perplexity_test
 from metrics.rouge import run_rouge
+from nlp.bias_detection import calculate_bias
 from nlp.contains_verb import run_contains_verb
 from nlp.de_en import run_language_percentage
+from nlp.sentiment_analysis import run_sentiment_analysis
 from nlp.upper_lower_case import run_upper_lower_case
 
 
@@ -11,13 +13,20 @@ def main():
     # create the results folder with timestamp
     output_folder = create_results()
 
-    #sentiment analysis
+    # Bias
+    calculate_bias(output_folder)
+
+    # sentiment analysis
     run_sentiment_analysis(output_folder)
+
     # Bleu
     calculate_bleu(output_folder)
 
     # Perplexity
     run_perplexity_test(output_folder)
+
+    # Deutsch-englisch
+    run_language_percentage(output_folder)
 
     # Rouge
     run_rouge(output_folder)
