c2c3af4cbcd75e2ca4c18a39b38b59563daa446c
Duong Nguyen <duongnt1010@gmail.com>
2024-02-04 22:56:49 +0100

    add confluence link zu bleu result description

--- create_results.py
+++ create_results.py
@@ -18,7 +18,7 @@ def create_results():
     data_structure = {
         "Metrics explanations": {
             "Rouge": "Rouge evaluates the ability of an LLM to generate summaries from an input text. The rouge score ranges from 0 to 1. A higher value indicates a better summary quality.",
-            "BLEU": "BLEU is a metric for evaluating the quality of machine translations. Bleu score ranges from 0 to 1. A higher BLEU score indicates that the automatic translations matches the reference translations better.",
+            "BLEU": "BLEU is a metric for evaluating the quality of machine translations. Bleu score ranges from 0 to 1. A higher BLEU score indicates that the automatic translations matches the reference translations better. Links zur Confluence: https://confluence.de.nortal.com/display/HTWSTUD/Metrics#Metrics-BLEUundsacreBLEU und https://confluence.de.nortal.com/display/HTWSTUD/Project+Approach#ProjectApproach-BLEUassessesamodel",
             "Perplexity": "Perplexity is a measure of the predictive uncertainty of a language model. A lower value indicates a higher prediction accuracy of the model. A value close to 50 is excellent."
         },
         "Results": []
