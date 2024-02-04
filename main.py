d5e6df44a171dff2631b36bf2beb88515cf7ca0c
Duong Nguyen <duongnt1010@gmail.com>
2024-02-04 16:31:49 +0100

    bias-detection: update bias with hate speech dataset, calculate the score of corect answer from openai, add result to output folder and add conclusion

--- main.py
+++ main.py
@@ -5,34 +5,39 @@ from metrics.rouge import run_rouge
 from nlp.contains_verb import run_contains_verb
 from nlp.de_en import run_language_percentage
 from nlp.upper_lower_case import run_upper_lower_case
+from nlp.bias_detection import calculate_bias
 
 
 def main():
     # create the results folder with timestamp
     output_folder = create_results()
 
+    # Bias
+    calculate_bias(output_folder)
+
     #sentiment analysis
     run_sentiment_analysis(output_folder)
+
     # Bleu
-    # calculate_bleu(output_folder)
-    #
-    # # Deutsch-englisch
-    # run_language_percentage(output_folder)
-    #
-    # # Perplexity
-    # # run_perplexity_test(output_folder)
-    #
-    # # Rouge
-    # run_rouge(output_folder)
-    #
-    # # Percentage of german & english words
-    # run_language_percentage(output_folder)
-    #
-    # # Upper lower case
-    # run_upper_lower_case(output_folder)
-    #
-    # # Conjugated Verb
-    # run_contains_verb(output_folder)
+    calculate_bleu(output_folder)
+
+    # Deutsch-englisch
+    run_language_percentage(output_folder)
+
+    # Perplexity
+   # run_perplexity_test(output_folder)
+
+    # Rouge
+    run_rouge(output_folder)
+
+    # Percentage of german & english words
+    run_language_percentage(output_folder)
+
+    # Upper lower case
+    run_upper_lower_case(output_folder)
+
+    # Conjugated Verb
+    run_contains_verb(output_folder)
 
 
 if __name__ == "__main__":
