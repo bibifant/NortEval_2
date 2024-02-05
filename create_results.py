41082318376e51130fb7f7051c5971348be2a139
Christina Gottschalk <christinaisa.g@gmail.com>
2024-02-05 15:43:50 +0100

    small changes in create_results.py

--- create_results.py
+++ create_results.py
@@ -22,7 +22,8 @@ def create_results():
             "Perplexity": "Perplexity is a measure of the predictive uncertainty of a language model. A lower value indicates a higher prediction accuracy of the model. A value close to 50 is excellent.",
             "Naturalness": "This score is calculated using perplexity. It assesses the fluency and coherence of the generated text. A lower score usually indicates text that is more natural and easy to read. A score below 50 is excellent.",
             "Semantic Similarity": "This score evaluates how relevant and contextually aligned the model's response is to the given prompt. The score ranges from 0 to 1. A higher score implies that in meaning the generated text is closely related to the prompt.",
-            "Keywords in Response": "This score measure the presence of key concepts in the response. The score ranges from 0 to 1. A higher score signifies that the response encompasses key elements from the prompt."
+            "Keywords in Response": "This score measures the presence of key concepts in the response. The score ranges from 0 to 1. A higher score signifies that the response encompasses key elements from the prompt.",
+            "Sentiment Analysis": "Sentiment analysis is a natural language processing task that involves determining and categorizing the emotional tone in a piece of text. \nCategories used: very positive, positive, neutral, negative, very negative. Scale ranges 0 - 100%. \nThe higher the percentage, the more sentiments have been identified correctly by the model. \nFor more information see https://confluence.de.nortal.com/display/HTWSTUD/Project+Approach#ProjectApproach-SentimentAnalysis"
         },
         "Results": []
     }
