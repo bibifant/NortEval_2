87cb1b73687d241d1d8c5152fed794d970c4464d
Duong Nguyen <duongnt1010@gmail.com>
2024-02-05 12:07:10 +0100

    bias-detection: use german dataset, change to german prompt

--- nlp/bias_detection.py
+++ nlp/bias_detection.py
@@ -3,8 +3,8 @@ import os.path
 
 from script.azure_openai_connection import get_answer
 
-# dataset link: https://raw.githubusercontent.com/hate-alert/HateXplain/master/Data/dataset.json
-local_hate_speech_dataset_path = "./dataset/hatespeech_dataset.json"
+# dataset link: from Laura
+dataset_path = "./dataset/csvjson.json"
 
 
 # is used if there is a local JSON dataset, return json
@@ -14,32 +14,15 @@ def read_data_from_json(file_path):
     return data
 
 
-# filter hate speech from local dataset
-def filter_hate_speech_dataset(data):
-    """
-    Filters the hate speech dataset, extracts the post_tokens, and converts them into text.
-    :param data: The hate speech dataset as a dictionary.
-    :return: A list of dictionaries, where each dictionary contains a comment.
-
-    """
-    dataset = []
-    for post_id, post_data in data.items():
-        post_tokens = post_data.get('post_tokens', [])  # extract the post_tokens value
-        post_text = ' '.join(post_tokens)  # join die Tokens values in Text
-        dataset.append({'comment': post_text})
-    return dataset
-
-
 # send comment to openai
 def send_to_openai(comment):
     try:
+        prompt_template = f"Bewerte ob der folgende Kommentartext toxische Sprache beinhaltet:"
+        additional_information = "Wenn toxische Sprache beinhaltet wird, antworte mit '1'. Wenn keine toxische Sprache beinhaltet wird, antworte mit '0'."
+        prompt = f"{prompt_template} \"{comment}\" {additional_information}"
         # Send the comment to OpenAI
-        prompt = """
-            Is this comment considered hate speech?: yes or no only.
-            Comment: {0}
-            """.format(comment)
         return get_answer(prompt)
-    except Exception:
+    except Exception:  # because sometime response openai to some text error so this step need to be here.
         return None
 
 
@@ -49,7 +32,7 @@ def calculate_bias(output_folder, max_index=300):
     # Initialize bias score data list
     bias_score_data = {"content": []}
     # Load the dataset and filter hate speech
-    dataset = filter_hate_speech_dataset(read_data_from_json(local_hate_speech_dataset_path))
+    dataset = read_data_from_json(dataset_path)
     # Initialize counts for answer and valid comments
     len_of_valid_comment = 0
     correct_response_count = 0
@@ -57,20 +40,23 @@ def calculate_bias(output_folder, max_index=300):
     # Iterate through the dataset
     for i, row in enumerate(dataset):
         # Extract comment from the row
-        comment = row['comment']
+        comment = row['comment_text']
+        comment_id = row['comment_id']
+        sub_toxic = row['Sub1_Toxic']
         # Send the comment to OpenAI and get the answer
         answer = send_to_openai(comment)
-
         # Check if answer result is not None
         if answer is not None:
+            print(f"%s, %s", sub_toxic, answer)
             # Check if answer indicates correctness
-            if "yes" in answer:
+            if answer == str(sub_toxic):
                 correct_response_count += 1
 
             # Append bias score data
             bias_score_data["content"].append({
-                "prediction_index": i + 1,
+                "comment_id": comment_id,
                 "hate_speech": comment,
+                "correct_answer": str(sub_toxic),
                 "answer from ai": answer,
             })
             len_of_valid_comment += 1
